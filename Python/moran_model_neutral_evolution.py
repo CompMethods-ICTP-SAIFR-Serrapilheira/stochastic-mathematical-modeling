c12677d0b0eec8d234fcd0b95837378c899568e3
Amanda Araujo <amanda_araujo@usp.br>
2022-08-21 22:36:46 -0300

    .

--- Python/moran_model_neutral_evolution.py
+++ Python/moran_model_neutral_evolution.py
@@ -74,3 +74,69 @@ plt.rcParams['figure.figsize'] = (32, 12)
 plt.rcParams.update({'font.size': 18})
 plt.title('Moran Process - Neutral Evolution; N = 100')
 plt.savefig('moran-neutral-N100-i50-Py.png')
+
+
+#-------------------------------------------------------------------------------
+# Probability of fixation 
+
+#Parameters
+N = 100  #pop size
+tmax = 10000 #number of interactions
+
+#Probability of fixation: simulation (exp)
+i = 0   #initial size of pop A
+di = 10
+i_sim = [] 
+P_sim = []
+
+n_runs = 100 #number of independent processes
+
+while i <= N:
+  i_sim.append(i)
+
+  fixation = 0
+
+  for j in range(n_runs):
+    values = neutral_evolution(N, i, tmax) #N, i, tmax
+    time = values[0]
+    popA = values[1]
+
+    #count how many fixated:
+    for a in popA:
+      if (a == N):
+        fixation += 1
+        break
+    
+  #probability of fixation for this initial value of i:
+  p_sim = fixation/n_runs
+  P_sim.append(p_sim)
+
+  i += di
+
+
+#Probability of fixation: theorical
+i = []
+P = []
+
+for j in range(N + 1):
+  P.append(j/N)
+  i.append(j)
+
+
+#Plot
+plt.plot(i, P)
+plt.plot(i_sim, P_sim)
+plt.xlim(0, N)
+plt.xlabel('Initial allele A population size (i)')
+plt.ylabel('Probability of fixation')
+plt.ylim(0, 1)
+plt.rcParams['figure.figsize'] = (12, 6)
+plt.rcParams.update({'font.size': 18})
+plt.title('Moran Process - Neutral Evolution; N = 100')
+plt.legend(['Theorical', 'Simulation: 100 runs, 10000 iterations'])
+plt.savefig('moran-neutral-N100-Pfixation-Py.png')
+
+plt.show()
+
+
+
